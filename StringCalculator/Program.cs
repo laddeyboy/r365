b3bbf1e327dd87fed8f3e8ec818482896823b525
Josh Ladwig <joshua.j.ladwig@gmail.com>
2025-03-11 09:57:07 -0500

    feat: Stretch Goal2 - Completed

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -1,11 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Please provide input: ");
-var input = Console.ReadLine();
-try
+Console.WriteLine("Press Ctrl+C to exit.");
+while (true)
 {
-  var result = StringCalculator.Calculate(input);
+  Console.Write("Please provide input: ");
+  var input = Console.ReadLine();
+  try
+  {
+    var result = StringCalculator.Calculate(input);
+  }
+  catch (Exception ex)
+  {
+    Console.WriteLine(ex.Message);
+  }
 }
-catch (Exception ex)
-{
-  Console.WriteLine(ex.Message);
-}
+
--- StringCalculator/obj/Debug/net8.0/StringCalculator.AssemblyInfo.cs
+++ StringCalculator/obj/Debug/net8.0/StringCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa1d90f83a573113b4157b6c449a92012460118e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63e10dc5e7e6ff18b08265016fa111bab3b622cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
