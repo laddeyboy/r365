e0317c5cc660d450e61e2b453c6ad176d2b45b7c
Josh Ladwig <joshua.j.ladwig@gmail.com>
2025-03-10 20:03:37 -0500

    feat: added gitignore file

--- StringCalculator/obj/Debug/net8.0/StringCalculator.AssemblyInfo.cs
+++ StringCalculator/obj/Debug/net8.0/StringCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d278254141d5d87331e1d266049c329789f836f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
